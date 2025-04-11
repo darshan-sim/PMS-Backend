315f0ff85b99b0ebaa3298d1f8493cf8f7bb5843
darshan-sim <darshan.chaudhary@simformsolutions.com>
2025-04-11 16:04:19 +0530

    chor: added prisma and tested a it on local machine

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,30 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import prisma from "./prisma";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 
+dotenv.config();
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(helmet());
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/health", (req: Request, res: Response) => {
-    res.send("Heart is betting");
+app.get("/users", async (req: Request, res: Response) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+});
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, World!");
 });
 
 app.use(errorHandler);
