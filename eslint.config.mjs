37b62207021bb9be13bac48c79c6d434303bdf85
darshan-sim <darshan.chaudhary@simformsolutions.com>
2025-05-19 18:29:32 +0530

    chor: added required types, nodemon, few eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,24 +5,26 @@ import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 export default defineConfig([
-    { ignores: ['node_modules/', 'dist/', 'build/'] },
-    { files: ['**/*.{js,mjs,cjs,ts}'] },
-    {
-        files: ['**/*.{js,mjs,cjs,ts}'],
-        languageOptions: { globals: globals.node },
-    },
-    {
-        files: ['**/*.{js,mjs,cjs,ts}'],
-        plugins: { js },
-        extends: ['js/recommended'],
-    },
-    tseslint.configs.recommended,
+  { ignores: ['node_modules/', 'dist/', 'build/'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  tseslint.configs.recommended,
 
-    prettierConfig,
-    {
-        plugins: { prettier: prettierPlugin },
-        rules: {
-            'prettier/prettier': 'error',
-        },
-    },
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+        'quotes': ['error', 'single'], 
+        'prettier/prettier': 'error',
+        '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+    }
+  },
 ]);
